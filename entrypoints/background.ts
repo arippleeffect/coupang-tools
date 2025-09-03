0cc5aad1ed2046dd427cffa4937f9a6c2c120590
김재훈 <kimjaehoon.pg@gmail.com>
2025-09-03 16:25:55 +0900

    feat: 로켓그로스 반출 액셀 다운로드 메뉴 추가

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,8 @@
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  // 페이지 우클릭 메뉴
+  browser.contextMenus.create({
+    id: "page-action",
+    title: "로켓그로스 반출 액셀 다운로드",
+    contexts: ["page"],
+  });
 });
