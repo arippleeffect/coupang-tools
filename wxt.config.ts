c57f6ad1c6524d3a6f0a7efa8acf0c38e3492778
김재훈 <kimjaehoon.pg@gmail.com>
2025-09-03 16:00:41 +0900

    feat: 기본 manifest 설정

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  modules: ["@wxt-dev/module-react"],
+  manifestVersion: 3,
+  manifest: {
+    name: "coupang-tools",
+    version: "1.0",
+    permissions: ["contextMenus"],
+  },
 });
