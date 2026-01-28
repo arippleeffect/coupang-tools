f13e520ab0761240d514b6c1c605205eb22562cf
kimjaehoon-x <kimjaehoon.pg@gmail.com>
2026-01-28 10:26:51 +0900

    chore: 버전 1.0.1 -> 1.0.2

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   manifestVersion: 3,
   manifest: {
     name: "쿠팡 지표 분석",
-    version: "1.0.1",
+    version: "1.0.2",
     permissions: ["contextMenus", "cookies", "webNavigation", "storage"],
     host_permissions: ["https://wing.coupang.com/*"],
   },
